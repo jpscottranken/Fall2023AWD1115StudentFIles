98b3dbbff882b687ec323c6056e4da063c4db6a8
Jeff Scot <jpscott@ranken.edu>
2023-09-25 18:59:24 -0500

    AWD1115 Student Files

---
+++ student_download/ex_starts/Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+
+        //[Route("[action]/{num?}")]
+        //public IActionResult Countdown(int id = 0)
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        {
+            string contentString = "Counting down:\n";
+            for (int i = start; i >= end; i--)
+            {
+                contentString += i + "\n";
+            }
+
+            contentString += message;
+
+            return Content(contentString);
+        }
+    }
+}
